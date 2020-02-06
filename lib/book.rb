3cf49aade4f489862a6e4776edd315fa03706e18
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-06 04:31:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,34 @@
 class Book
 
   def initialize (title)
-end
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author_name (name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def page_count (index)
+    @index = index
+  end
+  
+  def index
+    @index
+  end
+  
+  def genre (genres)
+    @genres = genres
+  end
+  
+  def genres
+    @genres
+  end
 end
