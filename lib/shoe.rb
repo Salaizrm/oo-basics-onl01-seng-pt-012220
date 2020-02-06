ab74f23b8e1a926970924bde3a912924e63a4451
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-06 04:44:57 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,19 @@
+class Shoe
+  
+  attr_accessor :color, :size, :material
+  attr_reader :brand
+
+  def initialize (brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def condition
+end
+  
+
+
+end
