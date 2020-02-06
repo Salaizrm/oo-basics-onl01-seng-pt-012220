a3580282b7a8164a2e7d6e9382248971827ba160
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-06 04:50:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize (brand)
@@ -9,14 +9,6 @@ class Shoe
   
   def brand
     @brand
-  end
-  
-  def condition= (condition = "tattered")
-    @condition
-  end
-  
-  def condition
-    @condition
   end
 
   def cobble
