583bc35905ba309ca4363859eadb2d810f8f6f15
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-06 04:47:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize (brand)
@@ -11,14 +11,7 @@ class Shoe
     @brand
   end
   
-  def condition= (condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-  
+
 
 
 end
